a5f26597641a4967663fc0f79cf3667cb55c19dd
CuriousPhysicist <andrewhampson6@gmail.com>
2017-09-21 20:22:39 +0100

    Final replacement complete, all tests still green. First refactoring of test suite.

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -1,20 +1,12 @@
 require 'rails_helper'
 
 feature 'User authentication' do	
-	background do
-		user = create :user
-	end
 
 	scenario 'can log in from index via dynamic navbar' do
-		visit '/'
-		expect(page).to_not have_content('New Post')
+		
+		user = create :user
 
-		click_link 'Login'
-
-		fill_in 'Email', with: 'rubinator@ruby.com'
-		fill_in 'Password', with: 'illbeback'
-
-		click_button 'Log in'
+		sign_in_with user
 
 		expect(page).to have_content('Signed in successfully.')
 		expect(page).to_not have_content('Register')
@@ -22,13 +14,10 @@ feature 'User authentication' do
 	end
 
 	scenario 'can log out once logged in' do
-		visit '/'
-		click_link 'Login'
+		
+		user = create :user
 
-		fill_in 'Email', with: 'rubinator@ruby.com'
-		fill_in 'Password', with: 'illbeback'
-
-		click_button 'Log in'
+		sign_in_with user
 
 		click_link 'Logout'
 		expect(page).to have_content('You need to sign in or sign up before continuing')
